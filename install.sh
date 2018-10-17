27807f8ed50d09f877ad51bfec5d90b53be41264
yhu <hooby@sg-ai.com>
2018-10-17 03:31:29 +0000

    update to not sudo

--- install.sh
+++ install.sh
@@ -12,27 +12,28 @@ fi
 
 echo "Using $PYTHON"
 
-# install protoc
+# install protoc, if you have install protoc ,please comment it 
 (
 source scripts/install_protoc.sh
 )
 
 # install tensorflow models
 (
-git submodule update --init
+# git submodule update --init
 cd $MODELS_DIR
+# git checkout -- . && git pull origin master 
 cd research
 sed -i '516s/print num_classes, num_anchors/print(num_classes, num_anchors)/' object_detection/meta_architectures/ssd_meta_arch_test.py
 sed -i '147s/print /print(/' object_detection/dataset_tools/oid_hierarchical_labels_expansion.py
 sed -i '149s/labels_file"""$/[optional]labels_file""")/' object_detection/dataset_tools/oid_hierarchical_labels_expansion.py
 protoc object_detection/protos/*.proto --python_out=.
-sudo $PYTHON setup.py install
+$PYTHON setup.py install
 cd slim
-sudo $PYTHON setup.py install
+$PYTHON setup.py install
 cd $ROOT_DIR
 )
 
 # install this project
 (
-sudo $PYTHON setup.py install
+$PYTHON setup.py install
 )
